bbf60e2634febfda594236425e8bb181c67f3680
revostfw <sahay.rohan@gmail.com>
2016-03-03 03:40:31 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title><?php echo (gethostname()); ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="img/favicon.ico" type="image/x-icon" />
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="css/file-server.css">
+</head>
+<body>
+	<nav><nav class='navbar navbar-default'><a class='navbar-brand' href="#"><?php echo (gethostname()); ?> server</a></nav>
+</body>
+</html>
